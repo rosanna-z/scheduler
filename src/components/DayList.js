4573c56b02e328f3096253a52064510b4c778683
Rosanna Zhang <rosannazhang92@gmail.com>
2022-11-01 23:21:48 -0700

    Build DayList component that maps over an array of objects to produce an array of components, and wrote stories to test it

---
+++ src/components/DayList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import DayListItem from "./DayListItem";
+
+export default function DayList(props) {
+
+  return props.map((day) => {
+    <ul>
+      <DayListItem
+        key={day.id}
+        name={day.name}
+        spots={day.spots}
+        selected={day.name === props.day}
+        setDay={day.setDay}
+      />
+    </ul>;
+  });
+}
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,9 +3,18 @@ import "./DayListItem.scss"
 import classNames from "classnames";
 
 export default function DayListItem(props) {
+  function formatSpots() {
+    if (props.spots == 1) {
+      return "1 spot remaining"
+    }
+    else if (props.spots == 0) {
+      return "no spots remaining"
+    }
+  }
+
   const dayClass = classNames("day-list__item", {
     "--selected": props.selected,
-    "--full": props.spots == 0
+    "--full": props.spots == 0 // <--- ask mentor
   });
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
