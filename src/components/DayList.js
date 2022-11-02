acb0b17a3c19a4928dd211b7724b66175c211e3f
Rosanna Zhang <rosannazhang92@gmail.com>
2022-11-02 14:03:19 -0700

    Fixed formatSpots function and styling classNames

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,14 +4,14 @@ import DayListItem from "./DayListItem";
 export default function DayList(props) {
 
   return props.map((day) => {
-    <ul>
-      <DayListItem
-        key={day.id}
-        name={day.name}
-        spots={day.spots}
-        selected={day.name === props.day}
-        setDay={day.setDay}
-      />
-    </ul>;
+    // <ul>
+    //   <DayListItem
+    //     key={day.id}
+    //     name={day.name}
+    //     spots={day.spots}
+    //     selected={day.name === props.day}
+    //     setDay={day.setDay}
+    //   />
+    // </ul>;
   });
 }
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,23 +3,28 @@ import "./DayListItem.scss"
 import classNames from "classnames";
 
 export default function DayListItem(props) {
-  function formatSpots() {
-    if (props.spots == 1) {
+
+  function formatSpots(props) {
+    if (props.spots >= 2) {
+      return `${props.spots} spots remaining`
+    }
+    if (props.spots === 1) {
       return "1 spot remaining"
     }
-    else if (props.spots == 0) {
+    else if (props.spots === 0) {
       return "no spots remaining"
     }
   }
 
   const dayClass = classNames("day-list__item", {
-    "--selected": props.selected,
-    "--full": props.spots == 0 // <--- ask mentor
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
   });
+
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props)}</h3>
     </li>
   );
 }
