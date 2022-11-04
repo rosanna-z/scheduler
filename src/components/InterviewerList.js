713bef87b6cdeec8f3f9c70317f248441c8146fe
Rosanna Zhang <rosannazhang92@gmail.com>
2022-11-03 18:42:19 -0700

    Create Form component, add props and state, and update Appointment component to display Show or Empty component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,7 +3,7 @@ import "./InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
 
 export default function InterviewerList(props) {
-
+  
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
